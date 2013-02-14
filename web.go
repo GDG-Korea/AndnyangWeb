51f7cdda1998ec75f29ab5cc90448013f3d06293
Leonardo YongUk Kim <dalinaum@gmail.com>
2013-02-14 09:29:07 +0900

    implement a very rough template.

--- web.go
+++ web.go
@@ -4,7 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/bmizerany/pq"
-	_ "html/template"
+	"html/template"
 	"log"
 	"net/http"
 	"strings"
@@ -21,6 +21,12 @@ const (
 	TF_LOG = "12:30 PM"
 )
 
+const page = `
+{{range .}}
+<li>{{.}}</li>
+{{end}}
+`
+
 func logHandler(w http.ResponseWriter, r *http.Request) {
 	const lenPath = len(logPrefix)
 	queryString := r.URL.Path[lenPath:]
@@ -29,7 +35,7 @@ func logHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
-	db, error := sql.Open("postgres", "user=postgres password=gdg dbname=andnyang sslmode=disable")
+	db, error := sql.Open("postgres", "user=dalinaum password=gdg dbname=andnyang sslmode=disable")
 	if error != nil {
 		log.Print(error)
 		fmt.Fprintf(w, error.Error())
@@ -50,6 +56,8 @@ func logHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, error.Error())
 	}
 
+	templateRows := []string{}
+
 	for rows.Next() {
 		var id int
 		var date time.Time
@@ -64,8 +72,17 @@ func logHandler(w http.ResponseWriter, r *http.Request) {
 		localTime := date.Local()
 		hour := localTime.Hour()
 		min := localTime.Minute()
-		fmt.Fprintf(w, "<li>%d:%d [%s] %s</li>", hour, min, nick, message)
+		//fmt.Fprintf(w, "<li>%d:%d [%s] %s</li>", hour, min, nick, message)
+		line := fmt.Sprintf("%d:%d [%s] %s", hour, min, nick, message)
+		templateRows = append(templateRows, line)
 	}
+
+	t := template.New("hello template")
+	t, error = t.Parse(page)
+	if error != nil {
+		log.Print(error)
+	}
+	t.Execute(w, templateRows)
 }
 
 func main() {
