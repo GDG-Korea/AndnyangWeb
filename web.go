ffeb25868675ed22cfbdb2d9d43a6a3eef8a25ae
Leonardo YongUk Kim <dalinaum@gmail.com>
2013-02-15 13:08:21 +0900

    breaks into seperated logs by date.

--- web.go
+++ web.go
@@ -5,11 +5,12 @@ import (
 	"fmt"
 	_ "github.com/bmizerany/pq"
 	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 	"strings"
 	"time"
-	"io/ioutil"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
@@ -22,17 +23,20 @@ const (
 	TF_LOG = "12:30 PM"
 )
 
-const page = `
-{{range .}}
-<li>{{printf "%02d" .Hour}}:{{printf "%02d" .Min}} [{{.Nick}}] {{.Message}}</li>
-{{end}}
-`
-
 type Log struct {
-	Hour int
-	Min int
-	Nick string
+	Hour    int
+	Min     int
+	Nick    string
 	Message string
+}
+
+func getSurfixQuery(year int, month time.Month, day int) string {
+	const TF_SQL = "20060102 15:04:05"
+	const TF_CALENDAR = "20060102 15:04:05 -0700"
+	st, _ := time.Parse(TF_CALENDAR, fmt.Sprintf("%04d%02d%02d 00:00:00 +0900", year, month, day))
+	st = st.UTC()
+	et := st.AddDate(0, 0, 1)
+	return fmt.Sprintf(" and date between '%s' and '%s'", st.Format(TF_SQL), et.Format(TF_SQL))
 }
 
 func logHandler(w http.ResponseWriter, r *http.Request) {
@@ -51,12 +55,28 @@ func logHandler(w http.ResponseWriter, r *http.Request) {
 
 	channel := queries[0]
 
-	var sqlString string
 	if len(channel) == 0 {
-		// sqlString = "select * from andnyang_log"
-		fmt.Fprintf(w, "log viewer needs specified channel.")
+		return
+	}
+
+	sqlString := fmt.Sprintf("select * from andnyang_log where channel='#%s'", channel)
+
+	if len(queries) != 2 || len(queries[1]) != 8 {
+		now := time.Now().Local()
+		year := now.Year()
+		month := now.Month()
+		day := now.Day()
+		path := fmt.Sprintf("/log/%s/%04d%02d%02d", channel, year, month, day)
+		http.Redirect(w, r, path, http.StatusFound)
+		return
 	} else {
-		sqlString = fmt.Sprintf("select * from andnyang_log where channel='#%s'", channel)
+		dateQuery := queries[1]
+		year, _ := strconv.Atoi(dateQuery[0:4])
+		monthNumber, _ := strconv.Atoi(dateQuery[4:6])
+		month := time.Month(monthNumber)
+		day, _ := strconv.Atoi(dateQuery[6:])
+		surfix := getSurfixQuery(year, month, day)
+		sqlString = sqlString + surfix
 	}
 
 	rows, error := db.Query(sqlString)
@@ -80,12 +100,11 @@ func logHandler(w http.ResponseWriter, r *http.Request) {
 		localTime := date.Local()
 		hour := localTime.Hour()
 		min := localTime.Minute()
-		//fmt.Fprintf(w, "<li>%d:%d [%s] %s</li>", hour, min, nick, message)
-		//line := fmt.Sprintf("%d:%d [%s] %s", hour, min, nick, message)
-		log := Log {
-			Hour: hour,
-			Min: min,
-			Nick: nick,
+
+		log := Log{
+			Hour:    hour,
+			Min:     min,
+			Nick:    nick,
 			Message: message,
 		}
 		logs = append(logs, log)
@@ -94,8 +113,8 @@ func logHandler(w http.ResponseWriter, r *http.Request) {
 	filename := "log.html"
 	body, error := ioutil.ReadFile(filename)
 	if error != nil {
-		body = []byte(page)
-	}	
+		log.Print(error)
+	}
 
 	t := template.New("hello template")
 	t, error = t.Parse(string(body))
